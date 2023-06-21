48e60c76fb822fcae0bb4fc3120129da82041f47
Matthew Ko <iammatthewko114@gmail.com>
2023-06-21 22:42:44 +0800

    set up auth page

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -3,19 +3,27 @@ import React from 'react'
 import Link from 'next/link'
 
 export default function Header(){
+    const loggedIn = false
     return(
-        <Flex minWidth='max-content' alignItems='center' gap='2'>
+        <Flex minWidth='max-content' alignItems='center' gap='2' paddingX="4" paddingY="2" borderBottom="2px">
             <Box p='2'>
                 <Text fontSize='xl' fontWeight={700}>852Shepherd</Text>
             </Box>
             <Spacer />
-            <ButtonGroup gap='2'>
-                <Link href='/signup'>
-                    <Button colorScheme='teal' marginTop='2'>Sign Up</Button>
-                </Link>
-                <Link href='/login'>
-                    <Button colorScheme='teal' marginTop='2' marginRight='4'>Log in</Button>
-                </Link>
+            <ButtonGroup gap='2' alignItems='center' variant='outline'>
+                {
+                    loggedIn? (
+                        <Link href='/'>
+                            <Button>Logout</Button>
+                        </Link>
+                    ) : (
+                        <>
+                        <Link href='/auth'>
+                            <Button borderColor='#91FF64' borderWidth="2px" textColor="blue.900" rounded="full">Sign Up/Login</Button>
+                        </Link>
+                        </>
+                    )
+                }
             </ButtonGroup>
         </Flex>
     )
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,10 +2,11 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import Header from "../components/header"
+import theme from '@/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Header />
       <Component {...pageProps} />
     </ChakraProvider>
