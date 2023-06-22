064c8715b54f4a7d29fcf7077826119d5be82cdc
AaronLai <Beckwo1015@gmail.com>
2023-06-22 15:16:34 +0800

    finish chatting api

---
+++ backend/src/chatting/model.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from fastapi import APIRouter, Depends, File, Form, UploadFile
+from typing import Annotated
+
+
+
+class ChattingDTO(BaseModel):
+    projectId: str | None = None
+    userId: str | None = None
+    nameSpace: str | None = None
+    text: str | None = None
+
+
+
