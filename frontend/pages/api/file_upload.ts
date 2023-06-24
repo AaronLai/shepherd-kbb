2359c630c148b091814eed7d09d759ac6073d581
AaronLai <Beckwo1015@gmail.com>
2023-06-24 22:18:43 +0800

    changed to use server as a middle layer for uploading file

--- frontend/pages/api/file_upload.ts
+++ frontend/pages/api/file_upload.ts
@@ -1,28 +1,57 @@
 // pages/api/file_upload.ts
 import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
+import fs from 'fs';
+import FormData from 'form-data';
 import getConfig from 'next/config';
+import formidable from 'formidable';
 const { publicRuntimeConfig } = getConfig();
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-
-    const body = req.body
     if (req.method === 'POST') {
         try {
-            console.log("file upload ",body);
-            const response = await axios.post(`${publicRuntimeConfig.API_ENDPOINT}/builder/uploadFile`, body , { headers: {
-                "Content-Type": "multipart/form-data",
-                'token': req.headers['token']
 
-              }}
-            );
-            const { status, message } = response.data;
-            res.status(200).json({ status, message });
-            } catch (error : any) {
-                res.status(error.response.status).json({ error: error.message });
-                console.log(error.response.data);
+              const form = formidable({});
+              
+
+            form.parse(req, async (err, fields, files) => {
+            
+                if (err) {
+                    console.error('Error parsing form data:', err);
+                    res.status(500).json({ error: 'Internal Server Error' });
+                    return;
+                }
+
+                const file = fs.createReadStream(files.file[0].filepath);
+                const formData = new FormData();
+                formData.append('file', file, files.file[0].originalFilename);
+                formData.append('projectId', fields.projectId[0]);
+
+                const response = await axios.post(`${publicRuntimeConfig.API_ENDPOINT}/builder/uploadFile`, formData, {
+                    headers: {
+                        ...formData.getHeaders(),
+                        'token': req.headers["token"]
+                    },
+                });
+                
+                const { status, message } = response.data;
+                res.status(200).json({ status, message });
+            });
+        } catch (error) {
+
+            console.log(error.response?.data);
+
+            res.status(error.response?.status || 500).json({ error: error.message });
         }
     } else {
         res.status(405).json({ error: 'Method not allowed' });
     }
 }
+
+
+
+export const config = {
+    api: {
+      bodyParser: false,
+    },
+};
